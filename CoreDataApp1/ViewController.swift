a4d6d4b0f4175692c8a308c43c431d31c55e6fee
sunset12321215 <sunsetno08@gmail.com>
2018-10-31 10:10:39 +0700

    Fix

--- CoreDataApp1/ViewController.swift
+++ CoreDataApp1/ViewController.swift
@@ -12,9 +12,23 @@ import CoreData
 class ViewController: UIViewController {
 
     //  Ánh xạ
-    @IBOutlet weak var txt_Ten: UITextField!
-    @IBOutlet weak var txt_Que: UITextField!
-    @IBOutlet weak var lbl_Info: UILabel!
+    @IBOutlet private weak var txtTen: UITextField!
+    @IBOutlet private weak var txtQue: UITextField!
+    @IBOutlet private weak var lblInfo: UILabel!
+    
+    private func getManagedObjectContext() -> NSManagedObjectContext? {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return nil
+        }
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    //  Tạo Kho chứa các đối tượng
+    var context: NSManagedObjectContext? {
+        get {
+            return getManagedObjectContext()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,31 +36,41 @@ class ViewController: UIViewController {
 
     //  ------------------      Core Data       ------------------
     
-        //  Tạo Kho chứa các đối tượng
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-   
-    @IBAction func btn_Them(_ sender: Any) {
-        //  Tạo entity của đối tượng
-        let entity_Nguoi = NSEntityDescription.entity(forEntityName: "Nguoi", in: context)
-        let Nguoi = NSManagedObject.init(entity: entity_Nguoi!, insertInto: context)
-        //  Add thông tin vào thuộc tính
-        Nguoi.setValue(txt_Ten.text, forKey: "ten")
-        Nguoi.setValue(txt_Que.text, forKey: "que")
-        //  Save data
-        try! context.save()
-    }
-    
-    @IBAction func btn_Tim(_ sender: Any) {
-        //  Tạo request truy vấn dữ liệu
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Nguoi")
-        //  Tạo mảng chứa dữ liệu
-        let result = try! context.fetch(fetchRequest) as! [NSManagedObject]
-        //  In ra các thông tin
-        for item in result
-        {
-            print(item.value(forKey: "ten") as! String, item.value(forKey: "que") as! String)
+    struct const {
+        func NguoiSetValueForTenQue(Nguoi: NSManagedObject,Ten: String, Que: String){
+            Nguoi.setValue(Ten, forKey: "ten")
+            Nguoi.setValue(Que, forKey: "que")
         }
     }
     
+    @IBAction func handleAddButtonClick(_ sender: Any) {
+        //  Tạo entity của đối tượng
+        guard let context = context, let personEntity = NSEntityDescription.entity(forEntityName: "Nguoi", in: context) else { return }
+        let Nguoi = NSManagedObject.init(entity: personEntity, insertInto: context)
+        //  Add thông tin vào thuộc tính
+        let constSetValue = const()
+        constSetValue.NguoiSetValueForTenQue(Nguoi: Nguoi, Ten: txtTen.text ?? "", Que: txtQue.text ?? "")
+        //  Save data
+        do {
+            try context.save()
+        } catch {
+            print("Error Save")
+        }
+    }
+    
+    @IBAction func handleSearchButtonClick(_ sender: Any) {
+        //  Tạo request truy vấn dữ liệu
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Nguoi")
+        //  Tạo mảng chứa dữ liệu
+        do {
+            let result = try context?.fetch(fetchRequest) as! [NSManagedObject]
+            //  In ra các thông tin
+            for item in result {
+                print(item.value(forKey: "ten") ?? "", item.value(forKey: "que") ?? "")
+            }
+        } catch {
+            print("Error result")
+        }
+    }
 }
 
